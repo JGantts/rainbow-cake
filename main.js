4d6b5db702e911358c9fec624dc31281a84c5682
JGantts <jacobgantt@icloud.com>
2023-02-11 23:13:23 +0000

    fix

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-myButton1.onclick = function() {
-    let percentage = (Math.random() * 50) + 25
-    rainbowCakeImg.style.height = percentage + "%"
+function say(toSay) {
+    jsMessage.innerHTML = toSay
 }
+
+myButton1.onclick = function() {
+    say("myButton1.onclick begin")
+    let percentage = Math.random() * 10
+    rainbowCakeImg.style.height = percentage + "rem"
+    jsMessage.innerHTML
+    say("myButton1.onclick end")
+}
+
+say("main.js loaded")
