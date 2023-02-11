30615fcd74596e912abb4f479650ef5e7a516407
JGantts <jacobgantt@icloud.com>
2023-02-11 21:07:21 +0000

    PUBLIC -> docs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 var app = express();
 
-var staticPath = path.join(__dirname, '/PUBLIC');
+var staticPath = path.join(__dirname, '/docs');
 app.use(express.static(staticPath));
 
 app.listen(3000, function() {
